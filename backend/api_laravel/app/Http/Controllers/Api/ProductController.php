6f9ce1997599044481e564c6001dd90e484ea5f0
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-09-13 13:02:10 +0700

    api-laravel

---
+++ backend/api_laravel/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new ProductResource(Product::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'nama_produk'=>'required',
+            'keterangan'=>'required',
+            'harga'=>'required',
+            'jumlah'=>'required',
+        ]);
+
+        //response error validation
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        //save to database
+        $product = Product::create([
+            'nama_produk'=> $request->nama_produk,
+            'keterangan'=> $request->keterangan,
+            'harga'=> $request->harga,
+            'jumlah'=> $request->jumlah
+        ]);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'nama_produk'=>'required',
+            'keterangan'=>'required',
+            'harga'=>'required',
+            'jumlah'=>'required',
+        ]);
+
+        //response error validation
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        //update to database
+        $product->update([
+            'nama_produk'=> $request->nama_produk,
+            'keterangan'=> $request->keterangan,
+            'harga'=> $request->harga,
+            'jumlah'=> $request->jumlah
+        ]);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return new ProductResource($product);
+    }
+}
